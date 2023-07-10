3b9998a7147da00fd293f4a6eb1a2ea062c0dddc
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-07-10 23:29:40 +0200

    Implementirana metoda ToSortedArray(List<string> lista)

--- Lab3_z2/Program.cs
+++ Lab3_z2/Program.cs
@@ -4,9 +4,27 @@ namespace Lab3_z2
 {
     class Program
     {
+        static List<string> ToSortedArray(List<string> lista)
+        {
+            var sortiranaLista = lista.OrderBy(s => s.Length).ToList();
+            return sortiranaLista;
+        }
+
         static void Main(string[] args)
         {
+            List<string> lista = new List<string>() {
+                "frizura",
+                "oko",
+                "ja",
+                "otorinolaringologija",
+                "globus",
+                "ti"
+            };
+            var sortiranaLista = ToSortedArray(lista);
 
+            Console.WriteLine("Stringovi sortirani prema dužini:");
+            foreach (var s in sortiranaLista)
+                Console.Write("{0}  ", s);
         }
     }
 }
